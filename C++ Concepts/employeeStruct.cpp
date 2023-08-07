cc2a61fea0e9d4f0b3687f72239a49ee6855b544
DhyanShah22 <dhyanshah229@gmail.com>
2023-08-07 09:47:27 +0530

    Day-9

--- C++ Concepts/employeeStruct.cpp
+++ C++ Concepts/employeeStruct.cpp
@@ -7,11 +7,24 @@ struct Employee{
     int EID;
     float EXP;
     char G;
-};
+} e3;
 
 int main() 
 {
     struct Employee e1;
+    struct Employee e2;
+
+    e2.Name= "DhyanShah";
+    e2.EID= 456;
+    e2.EXP=6.5;
+    e2.G= 'M';
+
+    e3.Name= "DhyanAmitShah";
+    e3.EID= 466;
+    e3.EXP=4.5;
+    e3.G= 'M';
+
+
     cout<<"Enter Employee name"<<endl;
     cin>>e1.Name;
     cout<<"Enter Employee user id."<<endl;
@@ -27,5 +40,15 @@ int main()
     cout<<"The Employee user id is: "<<e1.EID<<endl;
     cout<<"The employee year of experience is: "<<e1.EXP<<endl;
     cout<<"The gender is: "<<e1.G<<endl;
+
+    cout<<"The name of the employee is: "<<e2.Name<<endl;
+    cout<<"The Employee user id is: "<<e2.EID<<endl;
+    cout<<"The employee year of experience is: "<<e2.EXP<<endl;
+    cout<<"The gender is: "<<e2.G<<endl;
+
+    cout<<"The name of the employee is: "<<e3.Name<<endl;
+    cout<<"The Employee user id is: "<<e3.EID<<endl;
+    cout<<"The employee year of experience is: "<<e3.EXP<<endl;
+    cout<<"The gender is: "<<e3.G<<endl;
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+struct Employee
+{
+char name[20];
+int employeeId;
+double ex;
+char g;
+
+};
+
+void main(){
+struct Employee e;
+scanf("%s",&e.name);
+scanf("%d",&e.employeeId);
+scanf("%f",&e.ex);
+scanf("%s",&e.g);
+printf("%s",&e.name);
+printf("%d",&e.employeeId);
+printf("%f",&e.ex);
+printf("%s",&e.g);
+
+
+}
